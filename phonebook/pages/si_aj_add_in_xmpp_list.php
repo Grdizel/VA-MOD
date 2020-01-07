1b0028b19328348a98833b5db29e5e33c8c7ed8a
Grdizel <46874622+Grdizel@users.noreply.github.com>
2020-01-07 11:12:58 +0300

    Delete si_aj_add_in_xmpp_list.php

--- phonebook/pages/si_aj_add_in_xmpp_list.php
+++
@@ -1,4 +0,0 @@
-<?php
-    require_once("../config.php");
-    setcookie("xmpp_list[".(max(array_keys($_COOKIE['xmpp_list']))+1)."]", $_GET['login'], time()+$XMPP_MESSAGE_LISTS_TIME_OF_LIVE, "/");
-?>
