503387f037d97275edbf20be273c043ea3c47633
Grdizel <46874622+Grdizel@users.noreply.github.com>
2020-11-04 00:37:07 +0300

    1
    1

--- Digit_mod3.19/background.js
+++
@@ -1,9 +0,0 @@
-chrome.app.runtime.onLaunched.addListener(function() {
-  chrome.app.window.create('window.html', {
-    'bounds': {
-      'width': 285,
-      'height': 820
-    },alwaysOnTop: true
-  });
-
-});
